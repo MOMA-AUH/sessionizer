df3d13b2884d1d261343eab5e8f2a7bc50e782b5
simondrue <simondrue@gmail.com>
2024-01-09 08:55:04 +0100

    Handle the case when symlink already exists

--- src/sessionizer/utils.py
+++ src/sessionizer/utils.py
@@ -15,15 +15,21 @@ FILE_INDEX_EXTENSIONS = {
 
 def generate_symlink(shortcut_dir, file):
     symlink = os.path.join(shortcut_dir, os.path.basename(file))
-    os.symlink(src=file, dst=symlink)
+    try:
+        os.symlink(src=file, dst=symlink)
+    except FileExistsError:
+        pass
 
     # Handle index files if they exist (https://igvteam.github.io/igv-webapp/fileFormats.html)
     file_type = file.split(".")[-1]
     if file_type in FILE_INDEX_EXTENSIONS:
         file_index = file + FILE_INDEX_EXTENSIONS[file_type]
         if os.path.exists(file_index):
-            file_index_symlink = os.path.join(shortcut_dir, os.path.basename(file_index))
-            os.symlink(file_index, file_index_symlink)
+            try:
+                file_index_symlink = os.path.join(shortcut_dir, os.path.basename(file_index))
+                os.symlink(file_index, file_index_symlink)
+            except FileExistsError:
+                pass
 
     return symlink
 
