eb0d5daa7ba019d8ba61e67e07cfc214702c4388
simondrue <simondrue@gmail.com>
2024-01-22 15:20:58 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="sessionizer",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages("src"),
     package_dir={"": "src"},
     entry_points={"console_scripts": ["sessionizer = sessionizer.main:app"]},
