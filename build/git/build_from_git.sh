1b56e7c280b8be63534edee222e920625fc7cd16
simondrue <simondrue@gmail.com>
2024-01-22 15:40:18 +0100

    Update repo location in build script

--- build/git/build_from_git.sh
+++ build/git/build_from_git.sh
@@ -1,5 +1,5 @@
 # Build package from git
-BUILD_DIR="/faststorage/project/MomaReference/BACKUP/nanopore/software/conda"
+BUILD_DIR="/faststorage/project/MomaReference/BACKUP/nanopore/software/conda/repo/"
 conda build . --output-folder $BUILD_DIR
 
 # Clean up
